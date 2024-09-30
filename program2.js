b982063d227f17b6d0cd41cdf6041788da77544c
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-30 12:41:13 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -13,19 +13,6 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000,
   };
-
-  let total = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    // If the current value is less than the next value, subtract it from the total
-    if (i < s.length - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
-      total -= romanMap[s[i]];
-    } else {
-      total += romanMap[s[i]];
-    }
-  }
-
-  return total;
 };
 
 module.exports = { romanToInt };

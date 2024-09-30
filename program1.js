f22c82f2d7d692830a2bcf2d90b53fb09a5506b6
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-30 12:24:45 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,9 +3,17 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const map = { '(': ')', '{': '}', '[': ']' };
+
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(map[char]);
+        } else if (stack.length === 0 || stack.pop() !== char) {
+            return false;
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
